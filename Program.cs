bc20209558cd3d3252a0a5576f8aaa5ce721dfe2
Paulo Henrique de Andrade <phandradefilho@hotmail.com>
2024-10-02 15:15:53 -0300

    Implementações necessárias feitas e projeto finalizado.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone NOKIA");
+Smartphone nokia = new Nokia("1234", "Deluxe", "11111", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n----------------------------");
+
+Console.WriteLine("Smartphone IPHONE");
+Smartphone iphone = new Iphone("9876", "16 Pro Max", "22222", 246);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
+iphone.InstalarAplicativo("Brawl Stars");
